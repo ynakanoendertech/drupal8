a4f43dcd3a11a59e3d42ad5ae1e9930841028855
Yasuhiro Nakano <ynakano@endertech.com>
2016-03-16 18:17:36 -0700

    Add ExampleExtension.php and twig_demo.services.yml

---
+++ modules/custom/twig_demo/src/Twig/ExampleExtension.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\twig_demo\Twig;
+
+class ExampleExtension extends \Twig_Extension {
+
+    public function getName() {
+        return 'example';
+    }
+
+    public function getFunction() {
+        return array(
+            new \Twig_SimpleFunction(
+                'gravatar', array($this, 'generateGravatarUrl')
+            )
+        );
+    }
+
+    public function generateGravatarUrl($email, $size = 100) {
+        return sprintf('http://www.gravatar.com/avatar/%s?s=%s',
+            md5($email),
+            $size
+        );
+    }
+}
