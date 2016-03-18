5e483e9d8ce34b70915c8a3a57829ab6b2d554d5
Yasuhiro Nakano <ynakano@endertech.com>
2016-03-18 14:37:31 -0700

    Use getValue() of EntityReferenceFieldItemList

--- modules/custom/twig_demo/src/Twig/ExampleExtension.php
+++ modules/custom/twig_demo/src/Twig/ExampleExtension.php
@@ -12,7 +12,10 @@ class ExampleExtension extends \Twig_Extension {
         return array(
             new \Twig_SimpleFunction(
                 'gravatar', array($this, 'generateGravatarUrl')
-            )
+            ),
+            new \Twig_SimpleFunction(
+                'my_function', array($this, 'myFunction')
+            ),
         );
     }
 
@@ -21,5 +24,36 @@ class ExampleExtension extends \Twig_Extension {
             md5($email),
             $size
         );
+    }
+
+    public function myFunction($content) {
+        $outputArray = array();
+
+        foreach($content as $key1 => $value1) {
+            if ($key1 == '#view') {
+                foreach($value1 as $key2 => $value2) {
+                    if ($key2 == 'result') {
+                        foreach($value2 as $key3 => $value3) {
+
+                            $nodeFields = array();
+
+                            foreach($value3 as $key4 => $value4) {
+                                if ($key4 == '_entity') {
+                                    foreach($value4 as $key5 => $value5) {
+                                        $nodeFields[] = array($key5 => $value5->getValue());
+                                    }
+                                }
+                            }
+
+                            $outputArray[] = $nodeFields;
+                        }
+                    }
+                }
+            }
+        }
+
+        $output = json_encode($outputArray);
+
+        return $output;
     }
 }
