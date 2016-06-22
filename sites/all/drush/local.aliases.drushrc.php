b312f5b6e9585ba559f910cafcb72245b0372563
Yasuhiro Nakano <ynakano@endertech.com>
2016-06-21 17:19:01 -0700

    Add 8.1 version files manually

--- sites/all/drush/local.aliases.drushrc.php
+++
@@ -1,9 +0,0 @@
-<?php
-$aliases["8a"] = array(
-  'root' => '/Applications/MAMP/htdocs/drupal8a',
-  'uri' => 'dev.drupal8a.com',
-);
-$aliases["8b"] = array(
-  'root' => '/Applications/MAMP/htdocs/drupal8b',
-  'uri' => 'dev.drupal8b.com',
-);
